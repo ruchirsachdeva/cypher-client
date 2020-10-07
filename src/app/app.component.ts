df0e4a17a6c08b66207cfe727806919ac6f496fb
DwarakanathAkkala <37907049+DwarakanathAkkala@users.noreply.github.com>
2020-10-07 18:37:55 +0530

    Added different header and footer for mobile version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router }  from "@angular/router";
 import { event } from 'jquery';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,16 @@ import { event } from 'jquery';
 })
 export class AppComponent implements OnInit {
 
-  constructor(public router: Router){}
+  constructor(public router: Router,  private deviceService: DeviceDetectorService){
+    this.checkDevice();
+  }
+
   title = 'Cypher';
+
+  deviceInfo = null;
+  isMobile : boolean= false;
+  isTablet : boolean= false;
+  isDesktopDevice :  boolean= false;
 
   ngOnInit() {
      
@@ -24,4 +33,22 @@ export class AppComponent implements OnInit {
     event.stopPropagation();
   }
   
+  checkDevice (){
+    this.deviceInfo = this.deviceService.getDeviceInfo();
+    this.isMobile = this.deviceService.isMobile();
+    this.isTablet = this.deviceService.isTablet();
+    this.isDesktopDevice = this.deviceService.isDesktop();
+    console.log(this.isMobile);
+  }
+
+  openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+
+  closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  } 
+
 }
