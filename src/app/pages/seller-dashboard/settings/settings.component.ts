99cfbda3f54492bd7dd46ae63e1448271249f463
DwarakanathAkkala <37907049+DwarakanathAkkala@users.noreply.github.com>
2021-01-12 21:12:00 +0530

    Added settings page

---
+++ src/app/pages/seller-dashboard/settings/settings.component.ts
@@ -0,0 +1,57 @@
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../../../services/user.service";
+import {User} from "../../../models/User";
+import {Router } from "@angular/router";
+import {Observable, Subject} from "rxjs";
+import { Role } from '../../../enum/Role';
+
+@Component({
+    selector: 'app-settings',
+    templateUrl: './settings.component.html',
+    styleUrls: ['./settings.component.css']
+})
+export class SettingsComponent implements OnInit {
+
+    constructor(private userService: UserService,
+                private router: Router) {
+    }
+
+    user= new User();
+
+    isInvalid: boolean;
+    isLogout: boolean;
+    submitted = false;
+
+
+    ngOnInit() {
+    /* const account = this.userService.currentUserValue.account;
+
+        this.userService.get(account).subscribe( u => {
+            this.user = u;
+            this.user.password = '';
+        }, e => {
+
+      });*/
+    }
+
+    onSubmit() {
+        this.submitted = true;
+        this.userService.update(this.user).subscribe(u => {
+            this.userService.nameTerms.next(u.firstName);
+            let url = '/';
+            if (this.user.role != Role.Customer) {
+                url = '/seller';
+            }
+            this.router.navigateByUrl(url);
+        }, _ => {})
+    }
+
+    goToActive(){
+        this.router.navigate(['/active']);
+    }
+
+    goToHistory(){
+        this.router.navigate(['/history']);
+    }
+
+}
